114ff87389ab009f545eb985c8350ac8524fd22b
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2021-02-21 12:04:39 +0000

    [finish]8章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '36b488b67260252493386c2eff366c7b3ac6da3394fd2b1d3dcb0de24edf84688d702e3186419b6e46436f85f376ad8a5959bb9b2b2be75d27f9363e8bb3385c'
